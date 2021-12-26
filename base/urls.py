24b06fb878218ec01a6e79f5f2644cf7e52d64ac
Jackk-Doe <dodo-stv@hotmail.com>
2021-12-26 16:15:19 +1300

    Finished Theme design parts
    - Before implementing Django Rest API
    - Finished Theme parts
    - Fixed UI to mobile reponsesiveness

--- base/urls.py
+++ base/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
 
     # No need for ID (parameter) because the user gonna be the login-ed user
     path('update-user/', views.updateUser, name="update-user"),
+
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
